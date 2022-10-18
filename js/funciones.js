9a49b1a4d9936dec0510dbcaa25d322868f9e6b4
Francisco Panuccio <franciscopanuccio@hotmail.com>
2022-10-18 15:44:47 -0300

    Agregando funcionalidad

---
+++ js/funciones.js
@@ -0,0 +1,32 @@
+function dolar() {
+    setInterval(() => {
+        dolarFetch();
+    }, 3000)
+}
+
+function dolarFetch() {
+    fetch(dolaresUrl)
+    .then(response => response.json())
+    .then(({oficial, blue, solidario}) => {
+        hideSpinner()
+        Doficial.innerText = `${"$" + oficial.toFixed(2)}`;
+        Dblue.innerText = `${"$" + blue.toFixed(2)}`;
+        Dsolidario.innerText = `${"$" + solidario.toFixed(2)}`;
+        inputDolares.addEventListener("input", () => {
+            const conversion = (inputDolares.value * blue).toLocaleString();
+            inputPesos.value = conversion;
+        })
+        inputPesos.addEventListener("input", () => {
+            const conversion2 = (inputPesos.value / blue).toLocaleString();
+            inputDolares.value = conversion2;
+        })
+    })
+    .catch(error => console.error(error))
+    .finally(() => console.log("Proceso Finalizado"))
+}
+
+function hideSpinner() {
+    for(const spinners of spanDolar) {
+        spinners.className = "spinnerOut";
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,14 @@
+let divDolar = document.getElementById("divDolar");
+let spanDolar = document.querySelectorAll(".spinner");
+let Doficial = document.getElementById("oficial");
+let Dblue =document.getElementById("blue");
+let Dsolidario = document.getElementById("solidario");
+let inputPesos = document.getElementById("pesos");
+let inputDolares = document.getElementById("dolares");
+let changeButton = document.getElementById("switch");
+const dolaresUrl = "https://criptoya.com/api/dolar";
+
+dolar();
+
+
+
